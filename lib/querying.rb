cd8d80bf2a64ead1fe0616ed2b392cd32da7be08
Hunter Carlisle <huntingcarlisle@gmail.com>
2018-11-15 22:18:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,24 +1,61 @@
 def select_books_titles_and_years_in_first_series_order_by_year
-  "Write your SQL query here"
+  "
+  SELECT books.title, books.year
+  FROM books
+  WHERE series_id = 1
+  ORDER BY year;
+  "
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "
+  SELECT name, motto
+  FROM characters
+  ORDER BY LENGTH(motto) DESC
+  LIMIT 1;
+  "
 end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "
+  SELECT species, COUNT(*)
+  FROM characters
+  GROUP BY species
+  ORDER BY 2 DESC
+  LIMIT 1;
+  "
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "
+  WITH subgenres_by_series AS (
+    SELECT series.author_id AS ai, series.subgenre_id, subgenres.name AS sg
+    FROM subgenres
+    JOIN series ON subgenres.id = series.subgenre_id)
+  SELECT authors.name, subgenres_by_series.sg
+  FROM authors
+  JOIN subgenres_by_series ON authors.id = subgenres_by_series.ai;
+  "
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "
+  WITH filtered_characters AS (
+    SELECT series_id, COUNT(*) AS number
+    FROM characters
+    WHERE species = 'human'
+    GROUP BY series_id)
+  SELECT series.title
+  FROM series
+  JOIN filtered_characters ON series.id = filtered_characters.series_id
+  ORDER BY filtered_characters.number DESC
+  LIMIT 1;
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "
+  
+  "
 end
